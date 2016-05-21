fb880352609044e744f6f3afd445ccf79517f667
supershowwei <supershowwei@gmail.com>
2016-05-21 21:15:06 +0800

    complete AntiXSS use Server.HtmlEncode() sample code

---
+++ AntiXSSSamples/AntiXSSSamples/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AntiXSSSamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Xss(string message)
+        {
+            ViewBag.Message = Server.HtmlEncode(message);
+
+            return View("Index");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # antixss.samples
+
+- AntiXSS use Server.HtmlEncode()
+- use AntiXSS Api in 'antixss' branch
