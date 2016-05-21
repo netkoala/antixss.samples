e19f7af18500b2ce5466fcd11c24dd0e8976634a
supershowwei <supershowwei@gmail.com>
2016-05-21 21:21:09 +0800

    complete AntiXSS use AntiXSS Api sample code

--- AntiXSSSamples/AntiXSSSamples/Controllers/HomeController.cs
+++ AntiXSSSamples/AntiXSSSamples/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Security.Application;
 
 namespace AntiXSSSamples.Controllers
 {
@@ -18,7 +19,20 @@ namespace AntiXSSSamples.Controllers
         [ValidateInput(false)]
         public ActionResult Xss(string message)
         {
-            ViewBag.Message = Server.HtmlEncode(message);
+            //string body = Sanitizer.GetSafeHtml("<div onload=\"alert('xss');\"><script>alert('xss')</script></div>");
+            //輸出結果
+            //<html>
+            //<body>
+            //<div></div>
+            //</body>
+            //</html>
+
+            //body = Sanitizer.GetSafeHtmlFragment("<div onload=\"alert('xss');\"><script>alert('xss')</script></div>");
+            //輸出結果
+            //<div></div>
+
+            ViewBag.SafeHtmlMessage = Sanitizer.GetSafeHtml(message);
+            ViewBag.SafeHtmlFragmentMessage = Sanitizer.GetSafeHtmlFragment(message);
 
             return View("Index");
         }
